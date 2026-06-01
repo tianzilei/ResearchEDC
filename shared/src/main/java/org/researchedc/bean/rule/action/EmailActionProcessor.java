e1ec8aa664fb0e17e2a2ea9f129ccbf4271eb252
tianzilei <tianzilei@live.com>
2026-06-01 23:42:53 +0800

    Replace email/notification infrastructure with no-op stubs
    Replace 9 email/notification classes with empty stub implementations
    that preserve method signatures but send no emails:
    
    - EmailEngine: empty constructor, static getters return empty strings
    - OpenClinicaMailSender: all sendEmail() overloads return empty string
    - BulkEmailSenderService: empty class
    - EmailActionBean x2 (bean + domain): stub with getters/setters
    - EmailActionProcessor x2 (bean + domain): implements ActionProcessor, no-op execute()
    - NotificationActionBean: stub with getters/setters
    - NotificationActionProcessor: implements ActionProcessor, no-op execute()
    
    All callers compile against these stubs without changes. The system
    sends zero emails at runtime while maintaining full API compatibility.
    
    Verified: mvn clean compile -pl app -am ✅, ModulithVerificationTest ✅

--- shared/src/main/java/org/researchedc/bean/rule/action/EmailActionProcessor.java
+++ shared/src/main/java/org/researchedc/bean/rule/action/EmailActionProcessor.java
@@ -1,46 +1,8 @@
 package org.researchedc.bean.rule.action;
-
+import javax.sql.DataSource;
 import org.researchedc.bean.login.UserAccountBean;
 import org.researchedc.bean.managestudy.StudyBean;
-import org.researchedc.core.EmailEngine;
-import org.researchedc.exception.OpenClinicaSystemException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-
-import jakarta.mail.MessagingException;
-import javax.sql.DataSource;
-
 public class EmailActionProcessor implements ActionProcessor {
-
-    protected final Logger logger = LoggerFactory.getLogger(getClass().getName());
-    DataSource ds;
-    EmailEngine emailEngine;
-
-    public EmailActionProcessor(DataSource ds) {
-        this.ds = ds;
-    }
-
-    public void execute(RuleActionBean ruleAction, int itemDataBeanId, String itemData, StudyBean currentStudy, UserAccountBean ub, Object... arguments) {
-        HashMap<String, String> arg0 = (HashMap<String, String>) arguments[0];
-        sendEmail(ruleAction, ub, arg0.get("body"), arg0.get("subject"));
-    }
-
-    private void sendEmail(RuleActionBean ruleAction, UserAccountBean ub, String body, String subject) throws OpenClinicaSystemException {
-
-        logger.info("Sending email...");
-        try {
-            getEmailEngine().process(((EmailActionBean) ruleAction).getTo().trim(), EmailEngine.getAdminEmail(), subject, body);
-            logger.info("Sending email done..");
-        } catch (MessagingException me) {
-            logger.error("Email could not be sent");
-            throw new OpenClinicaSystemException(me.getMessage());
-        }
-    }
-
-    private EmailEngine getEmailEngine() {
-        emailEngine = emailEngine != null ? emailEngine : new EmailEngine(EmailEngine.getSMTPHost(), "5");
-        return emailEngine;
-    }
+    public EmailActionProcessor(DataSource ds) {}
+    public void execute(RuleActionBean ruleAction, int itemDataBeanId, String itemData, StudyBean currentStudy, UserAccountBean ub, Object... arguments) {}
 }
--- shared/src/main/java/org/researchedc/domain/rule/action/EmailActionProcessor.java
+++ shared/src/main/java/org/researchedc/domain/rule/action/EmailActionProcessor.java
@@ -1,104 +1,11 @@
 package org.researchedc.domain.rule.action;
-
+import javax.sql.DataSource;
 import org.researchedc.bean.login.UserAccountBean;
 import org.researchedc.bean.managestudy.StudyBean;
 import org.researchedc.bean.submit.ItemDataBean;
-import org.researchedc.core.EmailEngine;
-import org.researchedc.dao.hibernate.RuleActionRunLogDao;
-import org.researchedc.domain.rule.RuleSetRuleBean;
-import org.researchedc.exception.OpenClinicaSystemException;
 import org.researchedc.logic.rulerunner.ExecutionMode;
 import org.researchedc.logic.rulerunner.RuleRunner.RuleRunnerMode;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.mail.MailException;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.mail.javamail.MimeMessageHelper;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.StringTokenizer;
-
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.InternetAddress;
-import jakarta.mail.internet.MimeMessage;
-import javax.sql.DataSource;
-
 public class EmailActionProcessor implements ActionProcessor {
-
-    protected final Logger logger = LoggerFactory.getLogger(getClass().getName());
-    DataSource ds;
-    EmailEngine emailEngine;
-    JavaMailSenderImpl mailSender;
-    RuleActionRunLogDao ruleActionRunLogDao;
-    RuleSetRuleBean ruleSetRule;
-
-    public EmailActionProcessor(DataSource ds, JavaMailSenderImpl mailSender, RuleActionRunLogDao ruleActionRunLogDao, RuleSetRuleBean ruleSetRule) {
-        this.ds = ds;
-        this.mailSender = mailSender;
-        this.ruleSetRule = ruleSetRule;
-        this.ruleActionRunLogDao = ruleActionRunLogDao;
-    }
-
-    public RuleActionBean execute(RuleRunnerMode ruleRunnerMode, ExecutionMode executionMode, RuleActionBean ruleAction, ItemDataBean itemDataBean,
-            String itemData, StudyBean currentStudy, UserAccountBean ub, Object... arguments) {
-        switch (executionMode) {
-        case DRY_RUN: {
-            return ruleAction;
-        }
-
-        case SAVE: {
-            HashMap<String, String> arg0 = (HashMap<String, String>) arguments[0];
-            sendEmail(ruleAction, ub, arg0.get("body"), arg0.get("subject"));
-            RuleActionRunLogBean ruleActionRunLog =
-                new RuleActionRunLogBean(ruleAction.getActionType(), itemDataBean, itemDataBean.getValue(), ruleSetRule.getRuleBean().getOid());
-            ruleActionRunLogDao.saveOrUpdate(ruleActionRunLog);
-            return null;
-        }
-        default:
-            return null;
-        }
-    }
-
-    private void sendEmail(RuleActionBean ruleAction, UserAccountBean ub, String body, String subject) throws OpenClinicaSystemException {
-
-        logger.info("Sending email...");
-        try {
-            MimeMessage mimeMessage = mailSender.createMimeMessage();
-
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
-            helper.setFrom(EmailEngine.getAdminEmail());
-            helper.setTo(processMultipleImailAddresses(((EmailActionBean) ruleAction).getTo().trim()));
-            helper.setSubject(subject);
-            helper.setText(body);
-
-            mailSender.send(mimeMessage);
-            System.out.println("Sending Email thru Email Action");
-            logger.debug("Email sent successfully on {}", new Date());
-        } catch (MailException me) {
-            logger.error("Email could not be sent");
-            throw new OpenClinicaSystemException(me.getMessage());
-        } catch (MessagingException me) {
-            logger.error("Email could not be sent");
-            throw new OpenClinicaSystemException(me.getMessage());
-        }
-    }
-
-    private InternetAddress[] processMultipleImailAddresses(String to) throws MessagingException {
-        ArrayList<String> recipientsArray = new ArrayList<String>();
-        StringTokenizer st = new StringTokenizer(to, ",");
-        while (st.hasMoreTokens()) {
-            recipientsArray.add(st.nextToken());
-        }
-
-        int sizeTo = recipientsArray.size();
-        InternetAddress[] addressTo = new InternetAddress[sizeTo];
-        for (int i = 0; i < sizeTo; i++) {
-            addressTo[i] = new InternetAddress(recipientsArray.get(i).toString());
-        }
-        return addressTo;
-
-    }
-
+    public EmailActionProcessor(DataSource ds, Object mailSender, Object ruleActionRunLogDao, Object ruleSetRule) {}
+    public RuleActionBean execute(RuleRunnerMode m, ExecutionMode e, RuleActionBean a, ItemDataBean i, String s, StudyBean st, UserAccountBean u, Object... args) { return a; }
 }
